ac332f211432f9b33df27bb04cffd7d4b5894ef7
LSX-s-Software <2682191498@qq.com>
2022-05-13 20:50:03 +0800

    feat: 升级数据库时自动重建数据库

--- app/src/main/java/com/lsx/finalhomework/MyDBHelper.java
+++ app/src/main/java/com/lsx/finalhomework/MyDBHelper.java
@@ -4,23 +4,30 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.lsx.finalhomework.entities.BookService;
+
 public class MyDBHelper extends SQLiteOpenHelper {
 
     public MyDBHelper(Context context) {
-        super(context, "bookstore_app.db", null, 2);
+        super(context, "bookstore_app.db", null, 3);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS account");
+        db.execSQL("DROP TABLE IF EXISTS book");
+        db.execSQL("DROP TABLE IF EXISTS cart");
+        onCreate(db);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        // 账号表
         db.execSQL("CREATE TABLE account" +
-                "(_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "(id INTEGER PRIMARY KEY AUTOINCREMENT," +
                 "username VARCHAR(50)," +
                 "password CHAR(32))");
+        // 书本表
         db.execSQL("CREATE TABLE book" +
                 "(id INTEGER PRIMARY KEY AUTOINCREMENT," +
                 "category TINYINT," +
@@ -30,6 +37,16 @@ public class MyDBHelper extends SQLiteOpenHelper {
                 "isbn CHAR(13)," +
                 "description TEXT," +
                 "price DOUBLE)");
+        // 购物车表
+        db.execSQL("CREATE TABLE cart (" +
+                "id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                "account_id INTEGER NOT NULL," +
+                "book_id INTEGER NOT NULL," +
+                "quantity INTEGER NOT NULL," +
+                "CONSTRAINT cart_book_id_book_id FOREIGN KEY (book_id) REFERENCES book (id) ON DELETE CASCADE," +
+                "CONSTRAINT cart_account_id_account_id FOREIGN KEY (account_id) REFERENCES account (id) ON DELETE CASCADE)");
+        // 添加初始数据
+        BookService.initData(db);
     }
 
 }
